1605ed2507716863f499f7d762b268be9978d7f1
Antonio Figueroa <antonio_figueroa@dell.com>
2024-08-13 15:32:09 -0500

    - adds operator exercise results

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}+{b} is {sum}.");
+            Console.WriteLine($"{a}-{b} is {difference}.");
+            Console.WriteLine($"{a}*{b} is {product}.");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+
+            Console.WriteLine($"Let's calculate the area of your circle. Please provide a radius value:");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+        }
+
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow( radius, 2 );
         }
     }
 }
